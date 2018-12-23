fff882aa2dd3980108efca2956ca9d66accb0067
shufengwu5946 <594695686@qq.com>
2018-12-24 01:00:50 +0800

    material ui

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import Add from '@material-ui/icons/Add';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/Inbox';
+import Mui from './material-ui/Mui';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        文字按钮
+        <br/>
+        <Button color="primary">
+          Hello World
+        </Button>
+        <br/>
+        描边按钮
+        <br/>
+        <Button variant="outlined" color="primary">
+          Hello World
+        </Button>
+        <br/>
+        实心按钮
+        <br/>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+        <br/>
+        浮动按钮
+        <br/>
+        <Fab color="primary" size = "small">
+          <Add />
+        </Fab>
+        <List component = "nav">
+          <ListItem button = "true">
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="列表1" />
+          </ListItem>
+          <ListItem button = "true">
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="列表1" />
+          </ListItem>
+          <ListItem button = "true">
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="列表1" />
+          </ListItem>
+          <ListItem button = "true">
+            <ListItemIcon>
+              <InboxIcon />
+            </ListItemIcon>
+            <ListItemText primary="列表1" />
+          </ListItem>
+        </List>
+        <Mui>
+          
+        </Mui>
       </div>
     );
   }
---
+++ src/material-ui/Mui.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import RenderProps from './css-in-js/RenderProps';
+export default class Mui extends React.Component{
+    render(){
+        return(
+            <div>
+                <RenderProps></RenderProps>
+            </div>
+        );
+    }
+}
